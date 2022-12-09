7a533f576df9ea4c93576eba525a46699994fa88
Nor_Book <thanawat.srit@kmutt.ac.th>
2022-12-09 15:28:22 +0700

    Add Program

---
+++ Node.cs
@@ -0,0 +1,30 @@
+class Node<T>
+{
+    private T value;
+    private Node<T> next = null;
+
+    public Node(T value)
+    {
+        this.SetValue(value);
+    }
+
+    public void SetNext(Node<T> next)
+    {
+        this.next = next;
+    }
+
+    public Node<T> Next()
+    {
+        return this.next;
+    }
+
+    public T GetValue()
+    {
+        return this.value;
+    }
+
+    public void SetValue(T value)
+    {
+        this.value = value;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    static Queue<string> queue = new Queue<string>();
+    static void Main(string[] args)
+    {
+        Input();
+        ShowFile();
+    }
+    static void Input()
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "Exit")
+            {
+                break;
+            }
+            else
+            {
+                if (queue.GetLength() < 3)
+                {
+                    queue.Push(input);
+                }
+                else
+                {
+                    queue.Pop();
+                    queue.Push(input);
+                }
+            }
+        }
+    }
+    static void ShowFile()
+    {
+        for (int i = 0; i < queue.GetLength(); i++)
+        {
+            Console.WriteLine(queue.Get(i));
+        }
+    }
+}
